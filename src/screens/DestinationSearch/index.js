0cd81cf8360cc06a6566892d65228e81c4704886
NishantRout <nishantnarayanrout@gmail.com>
2021-02-27 20:39:07 +0530

    Destination Search Screen

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,11 +1,32 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, Text, TextInput, View } from "react-native";
 import styles from "./styles";
+import SearchResults from "../../../assets/data/search";
+import { Entypo } from "@expo/vector-icons";
 
 const DestinationSearch = () => {
+  const [inputText, setInputText] = useState("");
   return (
-    <View>
-      <Text>Ds</Text>
+    <View style={styles.container}>
+      {/* Input component */}
+      <TextInput
+        style={styles.textInput}
+        placeholder="Where are you going?"
+        value={inputText}
+        onChangeText={setInputText}
+      />
+      {/* List of destinations */}
+      <FlatList
+        data={SearchResults}
+        renderItem={({ item }) => (
+          <View style={styles.row}>
+            <View style={styles.iconContainer}>
+              <Entypo name={"location-pin"} size={27} />
+            </View>
+            <Text style={styles.locationText}>{item.description}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
--- src/screens/DestinationSearch/styles.js
+++ src/screens/DestinationSearch/styles.js
@@ -1,5 +1,33 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+  },
+  textInput: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderColor: "#ECEBED",
+  },
+  iconContainer: {
+    backgroundColor: "#F1F1F1",
+    padding: 8,
+    marginRight: 15,
+    borderRadius: 10,
+    borderWidth: 0.3,
+    borderColor: "#d4d4d4",
+  },
+  locationText: {
+    fontWeight: "600",
+    color: "#333",
+    fontSize: 16,
+  },
+});
 
 export default styles;
